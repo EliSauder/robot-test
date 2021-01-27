e652b3670f2bb63f1224c55d346fc5c95d0b1d49
EliSauder <24995216+EliSauder@users.noreply.github.com>
2021-01-26 18:55:55 -0800

    feat: builds

--- src/main/java/org/frcnomad/robot/Main.java
+++ src/main/java/org/frcnomad/robot/Main.java
@@ -1,10 +1,16 @@
 package org.frcnomad.robot;
 
+import org.frcnomad.lib.*;
+
 public final class Main {
 
     private Main() {}
     
     public static void main(String[] args) {
+
+        Greeter greeter = new Greeter();
+
+        System.out.println(greeter.getMessage());
         //nothing to implement
     }
     
